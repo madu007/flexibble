2bdaf6360783dd64193576c9a6fd07b8dc76ddf0
Austin okonkwo <austinokonkwo1995@gmail.com>
2023-06-29 14:27:36 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <section className="flex-stsrt flex-col paddings mb-16">
+            <h1>Categories</h1>
+            <h1>Post</h1>
+            <h1>LoadMores</h1>
+        </section>
+    )
+}
+
+export default Home;
